d0be2f5226dfc0cf621640cce2800d7a7055ece3
jennielakatos-sys <jennielakatos@gmail.com>
2025-11-16 16:56:07 +0200

    chore: format and lint fixes; add ESLint/Prettier configs (remove large files)

---
+++ eslint.config.cjs
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      parserOptions: { ecmaFeatures: { jsx: true } }
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      react: require('eslint-plugin-react'),
+      'react-hooks': require('eslint-plugin-react-hooks')
+    },
+    rules: {
+      'no-unused-vars': ['warn'],
+      'no-console': 'off',
+      semi: ['error', 'always'],
+      'react/prop-types': 'off',
+      'eqeqeq': ['error', 'always'],
+      'no-var': 'error',
+      'prefer-const': ['error'],
+      'curly': 'error',
+      'dot-notation': 'warn',
+      'no-implicit-coercion': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/no-explicit-any': ['warn'],
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-vars': 'error'
+    }
+  }
+];
